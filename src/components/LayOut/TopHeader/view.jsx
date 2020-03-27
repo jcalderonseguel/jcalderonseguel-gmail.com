6d54e9350f037aa4437047cbe4fe8989847d9085
Javier  Calderon <33453799+jcalderonseguel@users.noreply.github.com>
2020-03-27 20:27:56 -0300

    global reducer actions state

--- src/components/LayOut/TopHeader/view.jsx
+++ src/components/LayOut/TopHeader/view.jsx
@@ -1,18 +1,26 @@
 import React, { Component } from 'react'
 import { connect } from 'react-redux'
+import {
+    setCloseOpenMenu
 
+} from "../../Global/actions"
 
 
 class TopHeader extends Component {
 
  
+    toggleNavigation =() => {
+             
+        this.props.dispatch(setCloseOpenMenu(!this.props.isOpenLateral))
+        
+    }
 
     render() {
         return (
             <div className="row border-bottom">
                 <nav className="navbar navbar-static-top" role="navigation" style={{ marginBottom: 0 }}>
                     <div className="navbar-header">
-                        <span className="navbar-minimalize minimalize-styl-2 btn btn-primary" onClick={(e) => this.toggleNavigation(e)}><i className="fa fa-bars"></i> </span>
+                        <span className="navbar-minimalize minimalize-styl-2 btn btn-primary" onClick={this.toggleNavigation}><i className="fa fa-bars"></i> </span>
                         <form role="search" className="navbar-form-custom" method="post" action="#">
                             <div className="form-group">
                                 <input type="text" placeholder="Busqueda" className="form-control" name="top-search" id="top-search" />
@@ -32,7 +40,9 @@ class TopHeader extends Component {
     }
 }
 
-const mapStateToProps = (state) => ({});
+const mapStateToProps = (state) => ({
+    isOpenLateral: state.global.isOpenLateral
+});
 
 const mapDispatchToProps = (dispatch) => ({
     dispatch: (action) => { dispatch(action); },
