34ace0da38568cce3ca60aa0275b450d6232a3b5
Cristian Fernandez <cristianfc@lagash.com>
2020-03-27 16:52:06 -0300

    Se agrega logica step y se crean comoponentes para maqueteo

---
+++ src/components/Modules/NewPerson/Steps/FinishStep.jsx
@@ -0,0 +1,22 @@
+import React from "react";
+import PropTypes from "prop-types";
+
+/*const AccountStep = props => {
+  return <div></div>;
+};
+
+AccountStep.propTypes = {};
+
+export default AccountStep;*/
+
+class FinishStep extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {};
+  }
+  render() {
+    return <div>Step 3</div>;
+  }
+}
+
+export default FinishStep;
