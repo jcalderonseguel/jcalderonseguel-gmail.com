9cf839e8c6493621edd00d91b248c55878480657
Javier  Calderon <33453799+jcalderonseguel@users.noreply.github.com>
2020-03-30 13:58:04 -0300

    bootstrap & react-bootstrap

--- src/components/Modules/Maintainers/Countries/CountriesData.jsx
+++ src/components/Modules/Maintainers/Countries/CountriesData.jsx
@@ -1,4 +1,5 @@
 import React, { Component} from 'react'
+import { Row, Col } from 'react-bootstrap';
 
 class CountriesData extends Component {
 
@@ -6,23 +7,23 @@ class CountriesData extends Component {
         const key = `${item.name}${new Date().getTime()}`;
 
         return (
-               <div key={key} className="col-lg-4" >
+               <Col key={key} lg={4}  >
                  <div className="contact-box">
-                 <div className="col-4" style={{border:1, color:"red"}}>
-                     <img alt="" height="128" width="128" className="img-circle m-t-xs img-responsive" src={item.flag }></img> 
+                     <Row>
+                            <Col sm={4} >
+                                <div className="text-center">
+                                <img alt="" height="90" width="90" className="img-circle m-t-xs img-responsive" src={item.flag }></img> 
+                                            <div className="m-t-xs font-bold"> {item.name}</div>
+                                        </div>                   
+                            </Col>
+                                <Col sm ={8} >
+                                    <h3><strong>{item.region}</strong></h3>
+                                    <p><i className="fa fa-map-marker"></i> {item.subregion }</p>
+                                </Col>
+                                <div className="clearfix"></div>
+                     </Row>
                  </div>
-                    <div className="col-4">
-                        <div className="text-center">
-                              <div className="m-t-xs font-bold"> {item.name}</div>
-                           </div>
-                        </div>
-                    <div className="col-4">
-                         <h3><strong>{item.region}</strong></h3>
-                         <p><i className="fa fa-map-marker"></i> {item.subregion }</p>
-                    </div>
-                    <div className="clearfix"></div>
-                 </div>
-                </div>
+                </Col>
         )
     }
 
@@ -37,6 +38,7 @@ class CountriesData extends Component {
            
             <div className=" wrapper wrapper-content animated fadeInRight">
                 <div className="row">
+                                 
                 {
 
                         data.map((item, index) => this.box(item, index))
