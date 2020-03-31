874123784874f8da651bb35050a264db32dc9ab1
Aguayo Muñoz, Pedro <pedroam@lagash.com>
2020-03-31 19:02:36 -0300

    edited table and work with the same structure get Person

--- src/components/Modules/Admin/columns.jsx
+++ src/components/Modules/Admin/columns.jsx
@@ -1,30 +1,10 @@
 const columns = [
-
-    {
-        width: 200,
-        label: 'Name',
-        dataKey: 'name',
-    },
-    {
-        width: 200,
-        label: 'Last Name',
-        dataKey: 'lastName',
-    },
-    {
-        width: 200,
-        label: 'Date of Birth',
-        dataKey: 'dateOfBirth',
-    },
-    {
-        width: 200,
-        label: 'Marital Status',
-        dataKey: 'maritalStatus',
-    },
-    {
-        width: 120,
-        label: 'Natiionality',
-        dataKey: 'nationality',
-    },
-]
+    { id: 'personId', label: 'Person Id', minWidth: 170 },
+    { id: 'firstName', label: 'First Name', minWidth: 170 },
+    { id: 'lastName', label: 'Last Name', minWidth: 200 },
+    { id: 'fullName', label: 'Full Name', minWidth: 100 },
+    { id: 'gender', label: 'Gender', minWidth: 100 },
+    { id: 'detail', label: 'Detail', minWidth: 100 },
+];
 
 export default columns;
