18235f6695f6aba99a6381effb3fa66223b1bad5
diegoc <diegoc@lagash.com>
2020-04-01 21:23:17 -0300

    floatleftNavigationDownButton

--- src/components/Modules/NewPerson/navigation.jsx
+++ src/components/Modules/NewPerson/navigation.jsx
@@ -5,50 +5,46 @@ import Person from "../../../styles/formCreatePerson";
 function Navigation(props) {
     const {step, sendPerson, backStep, nextStep, isLoading} = props;
         return (
-            <div className="container" style={{ float: "left"/*marginLeft: 720*/}}>
+            <div >
                 <div className="row">
-                    <div className="col-1" style={{ float: "left"/*marginLeft: 720*/}}>
-                    <Button
-                        variant="contained"
-                        size="large"
-                        style={step <= 1? Person.createPersonStyle.btnDownDisabled: Person.createPersonStyle.buttonDown}
-                        disabled={step <= 1? true: false}
-                        onClick={() => backStep()}>
-                        Previous
-                    </Button>
-                    </div>
-                    <div className="col-1">
-                         <Button
-                            variant="contained"
-                            style={step > 2 ? Person.createPersonStyle.btnDownDisabled: Person.createPersonStyle.buttonDown}
-                            size="large"
-                            disabled={step > 2? true: false}
-                            onClick={() => nextStep()}>
-                            Next
-                        </Button>
-                    </div>
-                    <div className="col-1">
-                    <Button
-                        variant="contained"
-                        size="large"
-                        disabled={isLoading}
-                        style={Person.createPersonStyle.buttonDown}
-                        onClick={!isLoading ? () => sendPerson() : null}>
-                        {/* onClick={!isLoading ? handleSendPerson : null}> */}
-                        {isLoading ? 'Loading…' : 'Send'}
-                    </Button>
-                    </div>
-                    <div className="col-1">
-                    <Button
-                        variant="contained"
-                        style={Person.createPersonStyle.buttonDown}
-                        size="large"
-                        // onClick={() => this.stepCancel(0)}
-                        >
-                        Cancel
-                    </Button>
-                    </div>
-                    
+                    <div className="col-lg-10">
+                        <div style={{ float: "right", margin: 40 }}>
+                            <Button
+                                variant="contained"
+                                size="large"
+                                style={step <= 1? Person.createPersonStyle.btnDownDisabled: Person.createPersonStyle.buttonDown}
+                                disabled={step <= 1? true: false}
+                                onClick={() => backStep()}>
+                                Previous
+                            </Button>
+                                <Button
+                                    variant="contained"
+                                    style={step > 2 ? Person.createPersonStyle.btnDownDisabled: Person.createPersonStyle.buttonDown}
+                                    size="large"
+                                    disabled={step > 2? true: false}
+                                    onClick={() => nextStep()}>
+                                    Next
+                                </Button>
+                            <Button
+                                variant="contained"
+                                size="large"
+                                disabled={isLoading}
+                                style={Person.createPersonStyle.buttonDown}
+                                onClick={!isLoading ? () => sendPerson() : null}>
+                                {/* onClick={!isLoading ? handleSendPerson : null}> */}
+                                {isLoading ? 'Loading…' : 'Send'}
+                            </Button>
+                            <Button
+                                variant="contained"
+                                style={Person.createPersonStyle.buttonDown}
+                                size="large"
+                                // onClick={() => this.stepCancel(0)}
+                                >
+                                Cancel
+                            </Button>
+                            </div>
+                        
+                        </div>
                 </div>
             </div>
         );
