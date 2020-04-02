409a1b3afd1a96d8a5042c68ff7efcb5f4ca9533
nataliav-lagash <nataliav@lagash.com>
2020-04-02 00:47:06 -0300

    link del menu  se coloca azul

--- src/components/Modules/NewPerson/MenuHeader/MenuHeader.jsx
+++ src/components/Modules/NewPerson/MenuHeader/MenuHeader.jsx
@@ -10,9 +10,9 @@ class MenuHeader extends Component {
         <div className="container  ">
           <div className="col-lg-12 text-left mt-4 mb-3">
             <div className="steps">
-              <Nav class="nav nav-pills nav-fill ">
+              <Nav class="nav nav-pills nav-fill  ">
                 <Nav.Item>
-                  <Nav.Link eventKey="2" onClick={btnProfile}>
+                  <Nav.Link eventKey="1" onClick={btnProfile}>
                     1.Profile
                   </Nav.Link>
                 </Nav.Item>
